42b34d0f9f224bbf09ebbcdb6a6b9585d4b0e447
kordy21 <m.abdelhamed941@gmail.com>
2025-12-01 17:44:55 +0200

    kordy ------- 4 ------ 1/12

--- src/screens/SingleNew/sections/SocialMediaLinks.jsx
+++ src/screens/SingleNew/sections/SocialMediaLinks.jsx
@@ -3,27 +3,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faShareNodes, faPrint } from "@fortawesome/free-solid-svg-icons";
 import { BookmarkIcon } from "@heroicons/react/24/outline";
+import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 
 const SocialMediaLinks = ({ links = {}, onShare }) => {
   return (
-    <div className="flex items-center gap-4 justify-center md:justify-between">
+    <div className="flex items-center justify-center gap-4 md:justify-between">
       <button
         onClick={() =>
           alert(
             "اضغط Ctrl + D (أو Cmd + D على الماك) لإضافة الصفحة إلى المفضلة."
           )
         }
-        className="transition duration-300 hover:text-yellow-500"
+        className="text-xl text-gray-500 transition duration-300 hover:text-primary"
         title=" حفظ "
       >
-        <BookmarkIcon className="h-6 w-6 text-gray-500  duration-300  hover:text-primary" />
+        {/* <i class="fa-regular fa-bookmark */}
+        <FontAwesomeIcon icon={faBookmark} />
+
+        {/* <BookmarkIcon className="w-6 h-6 text-gray-500 duration-300 hover:text-primary" /> */}
       </button>
       {links.facebook && (
         <a
           href={links.facebook}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-500 text-xl transition duration-300 hover:text-primary"
+          className="text-xl text-gray-500 transition duration-300 hover:text-primary"
           title="شارك عبر الفيس بوك"
         >
           <FontAwesomeIcon icon={faFacebook} />
@@ -35,7 +39,7 @@ const SocialMediaLinks = ({ links = {}, onShare }) => {
           href={links.twitter}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-500 text-xl transition duration-300 hover:text-primary"
+          className="text-xl text-gray-500 transition duration-300 hover:text-primary"
           title=" شارك عبر تويتر"
         >
           <FontAwesomeIcon icon={faXTwitter} />
@@ -44,7 +48,7 @@ const SocialMediaLinks = ({ links = {}, onShare }) => {
 
       <button
         onClick={() => window.print()}
-        className="text-gray-500 text-xl transition duration-300 hover:text-green-600"
+        className="text-xl text-gray-500 transition duration-300 hover:text-green-600"
         title=" اطبع الأن"
       >
         <FontAwesomeIcon icon={faPrint} />
@@ -52,7 +56,7 @@ const SocialMediaLinks = ({ links = {}, onShare }) => {
 
       <button
         onClick={onShare}
-        className="text-gray-500 text-xl transition duration-300 hover:text-green-600"
+        className="text-xl text-gray-500 transition duration-300 hover:text-green-600"
         title=" شارك الأن  "
       >
         <FontAwesomeIcon icon={faShareNodes} />
